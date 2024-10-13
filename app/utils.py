d539de5ceec6c2221e94569ee53e988c50ce1976
Bastien <bastien.pouessel@epita.fr>
2024-10-12 21:09:23 -0700

    refactor: update lisibility of the logger formatter

--- app/utils.py
+++ app/utils.py
@@ -1,14 +1,13 @@
+"""Utility functions."""
+
 import logging
 from datetime import UTC, datetime
-from typing import Any
-
-from fastapi import Request
 
 
 class LoggingFormatter(logging.Formatter):
     """Custom formatter for logging."""
 
-    def format(self, record: dict[str, Any]) -> str:
+    def format(self, record: logging.LogRecord) -> str:
         """Format the record to a string.
 
         Args:
@@ -20,11 +19,17 @@ class LoggingFormatter(logging.Formatter):
         timestamp = datetime.now(UTC).astimezone().isoformat()
 
         log_msg = (
-            f'time={timestamp} level={record.levelname} msg="{record.getMessage()}"'
+            f"time={timestamp}"
+            f" level={record.levelname}"
+            f" msg='{record.getMessage()}'"
         )
 
-        if hasattr(record, "request"):
-            req: Request = record.request
-            log_msg += f' ip={req.client.host}:{req.client.port} proto={req.scope["scheme"].upper()}/{req.scope["http_version"]} method={req.method} uri={req.url.path}'
+        if hasattr(record, "request") and (req := record.request):
+            log_msg += (
+                f" ip={req.client.host}:{req.client.port}"
+                f" proto={req.scope['scheme'].upper()}/{req.scope['http_version']}"
+                f" method={req.method}"
+                f" uri={req.url.path}"
+            )
 
         return log_msg
